f157cdb1dc7d5dd2d7d2f3e7fe73e0207a89abe6
KyleCSharp <kylelrigler@gmail.com>
2022-09-23 18:47:27 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("give me a number to convert F to C");
+            var celcius = TempConverter.FahrenheitToCelsius(int.Parse(Console.ReadLine())); //used console.readline for fun
+            Console.WriteLine($"celcius = {celcius}");
+
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(0);
+            Console.WriteLine($"fahrenheit = {fahrenheit}");
+        }
+    }
+}
